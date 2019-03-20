f5f1568a8790a56772d3f15ddacae3a0911b7db5
Calvo Torres <mcalvot@minsait.com>
2019-03-20 12:46:45 +0100

    first commit

---
+++ src/main/java/org/mcalvot/formacion/pilotoSpringBoot/PilotoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.mcalvot.formacion.pilotoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotoSpringBootApplication.class, args);
+	}
+
+}
