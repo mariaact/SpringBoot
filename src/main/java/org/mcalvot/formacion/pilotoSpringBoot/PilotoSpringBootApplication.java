3fe7aec16ee0dcef5c6a24fff329715f39660c11
mcalvot <Esma2019*>
2019-03-20 16:14:18 +0100

    He añadido el controlador rest que te saluda

--- src/main/java/org/mcalvot/formacion/pilotoSpringBoot/PilotoSpringBootApplication.java
+++ src/main/java/org/mcalvot/formacion/pilotoSpringBoot/PilotoSpringBootApplication.java
@@ -1,5 +1,6 @@
 package org.mcalvot.formacion.pilotoSpringBoot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/org/mcalvot/formacion/pilotoSpringBoot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.mcalvot.formacion.pilotoSpringBoot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping ("/")
+    public String index(){
+
+        return "HELLO";
+    }
+
+
+}
